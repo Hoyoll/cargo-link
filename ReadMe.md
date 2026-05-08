c3628b6e16b1c2ce30e6f6c836d5c22078f012a4
Effendi <effendi.saputra01@gmail.com>
2026-05-08 23:20:10 +0700

    it is usable

---
+++ ReadMe.md
@@ -0,0 +1,9 @@
+# How to use
+```	
+	// will create a .link folder
+	cargo-link init
+	// will act like ls but just list every dir or file name under that path
+	cargo-link <path>
+	// will create a symlink of said path and put it in your project
+	cargo-link <path> register <the-picked-dir-or-file>
+```
--- src/main.rs
+++ src/main.rs
@@ -21,97 +21,161 @@ fn symlink<P: AsRef<Path>, Q: AsRef<Path>>(
 
 use std::io::{self, BufRead};
 
-fn get_user_crate(args: &[String]) -> Option<String> {
-    if let Some(c) = args.get(2) {
-        return Some(c.clone());
-    }
-    let stdin = io::stdin();
-    for line in stdin.lock().lines() {
-        if let Ok(content) = line {
-            return Some(content);
-        }
-    }
+fn get_user_lib(args: Option<&String>) -> Option<String> {
+	match args {
+		Some(c) => {
+			return Some(c.clone())
+		}
+		None => {
+		    let stdin = io::stdin();
+		    for line in stdin.lock().lines() {
+		        if let Ok(content) = line {
+		            return Some(content);
+		        }
+		    }		
+		}
+	}
     None
 }
 
 fn main() {
-	let mut args: Vec<String> = std::env::args().collect();
-	match args.get(1) {
-		None => {}
-		Some(arg) => {
-			match arg.as_str() {
-				"list" => {
-					match home::cargo_home() {
-						Err(_) => {
-							println!("[Error]: Could not found cargo!")
-						}
-						Ok(mut cargo_home) => {
-						    cargo_home.push("registry");
-						   	cargo_home.push("src");
-							match std::fs::read_dir(cargo_home) {
-						    	Ok(dir) => {
-						    		for entry in dir {
-						    			let entry = entry.unwrap();
-						    			let dir_name = entry.path();
-						    			if !dir_name.file_name().unwrap().to_str().unwrap().starts_with("index.crates.io") {
-						    				continue
-						    			}
-						    			match std::fs::read_dir(dir_name) {
-						    				Err(_) => {}
-						    				Ok(dir) => {
-						    					for entry in dir {
-						    						let entry = entry.unwrap();
-						    						let dir_name = entry.path();
-													println!("{}", dir_name.file_name().unwrap().display())
-						    					}	
-						    				}
-						    			} 
-						    		}
-						    	}
-						    	Err(_) => ()
-						    }
-						}
-					}		
+	let args: Vec<String> = std::env::args().collect();
+	if args.len() == 1 {
+		println!("Please atleast give us a path to a dir...");
+		return;
+	}
+	let first_arg = args[1].clone();
+	match first_arg.as_str() {
+		"init" => {
+			match std::fs::create_dir_all(".link") {
+				Ok(_) => {
+					println!("Initialized .link folder")			
 				}
-				"custom" => {
+				Err(e) => {
+					println!("Error!: {}", e);
 				}
-				"register" => {
-					match get_user_crate(&args) {
+			}
+		}
+		path => {
+			match std::fs::read_dir(path) {
+				Err(_) => {
+					println!("Path [{}] does not exist!", &args[1]);
+				}
+				Ok(read_dir) => {
+					match args.get(2).map(String::as_str) {
 						None => {
-							println!("here!");
-						},
-						Some(user_crate) => {
-							match home::cargo_home() {
-								Err(_) => {
-									println!("[Error]: Could not found cargo!")
+							for entry in read_dir {
+	    						let entry = entry.unwrap();
+	    						let dir_name = entry.path();
+								println!("{}", dir_name.file_name().unwrap().display())
+	    					}
+						}
+						Some("register") => {
+							match get_user_lib(args.get(3)) {
+								None => {
+									println!("Atleast give us a name of your lib!");
 								}
-								Ok(mut cargo_home) => {
-								    cargo_home.push("registry");
-								   	cargo_home.push("src");
-									match std::fs::read_dir(cargo_home) {
-								    	Ok(dir) => {
-								    		for entry in dir {
-								    			let entry = entry.unwrap();
-								    			let mut dir_name = entry.path();
-								    			if !dir_name.file_name().unwrap().to_str().unwrap().starts_with("index.crates.io") {
-								    				continue
-								    			}
-								    			dir_name.push(&user_crate);
-								    			std::fs::create_dir_all(".link");
-								    			let mut path = std::path::PathBuf::from(".link");
-								    			path.push(&user_crate);
-								    			symlink(dir_name, path);
-								    		}
-								    	}
-								    	Err(_) => ()
-								    }
+								Some(lib) => {
+					    			let mut path = std::path::PathBuf::from(".link");
+					    			path.push(&lib);
+					    			let mut crate_path = std::path::PathBuf::from(&first_arg);
+									crate_path.push(&lib);
+									match symlink(&crate_path, &path) {
+										Err(e) => {
+											println!("Error!: {}", e);
+										}
+										Ok(_) => {
+											println!("Successfully created a symlink for {} in {}", crate_path.display(), path.display());
+										}
+									}
 								}
 							}
 						}
-					}					
+						Some(s) => {
+							println!("Error: Unknown argument {}!", s);
+						}
+					}
 				}
-				_ => ()			
-			}
+			}		
 		}
 	}
-}			
+}
+
+// fn _main() {
+// 	let mut args: Vec<String> = std::env::args().collect();
+// 	match args.get(1) {
+// 		None => {}
+// 		Some(arg) => {
+// 			match arg.as_str() {
+// 				"list" => {
+// 					match home::cargo_home() {
+// 						Err(_) => {
+// 							println!("[Error]: Could not found cargo!")
+// 						}
+// 						Ok(mut cargo_home) => {
+// 						    cargo_home.push("registry");
+// 						   	cargo_home.push("src");
+// 							match std::fs::read_dir(cargo_home) {
+// 						    	Ok(dir) => {
+// 						    		for entry in dir {
+// 						    			let entry = entry.unwrap();
+// 						    			let dir_name = entry.path();
+// 						    			match std::fs::read_dir(dir_name) {
+// 						    				Err(_) => {}
+// 						    				Ok(dir) => {
+// 						    					for entry in dir {
+// 						    						let entry = entry.unwrap();
+// 						    						let dir_name = entry.path();
+// 													println!("{}", dir_name.file_name().unwrap().display())
+// 						    					}	
+// 						    				}
+// 						    			} 
+// 						    		}
+// 						    	}
+// 						    	Err(_) => ()
+// 						    }
+// 						}
+// 					}		
+// 				}
+// 				"custom" => {
+// 				}
+// 				"register" => {
+// 					match get_user_crate(&args) {
+// 						None => {
+// 							println!("here!");
+// 						},
+// 						Some(user_crate) => {
+// 							match home::cargo_home() {
+// 								Err(_) => {
+// 									println!("[Error]: Could not found cargo!")
+// 								}
+// 								Ok(mut cargo_home) => {
+// 								    cargo_home.push("registry");
+// 								   	cargo_home.push("src");
+// 									match std::fs::read_dir(cargo_home) {
+// 								    	Ok(dir) => {
+// 								    		for entry in dir {
+// 								    			let entry = entry.unwrap();
+// 								    			let mut dir_name = entry.path();
+// 								    			if !dir_name.file_name().unwrap().to_str().unwrap().starts_with("index.crates.io") {
+// 								    				continue
+// 								    			}
+// 								    			dir_name.push(&user_crate);
+// 								    			std::fs::create_dir_all(".link");
+// 								    			let mut path = std::path::PathBuf::from(".link");
+// 								    			path.push(&user_crate);
+// 								    			symlink(dir_name, path);
+// 								    		}
+// 								    	}
+// 								    	Err(_) => ()
+// 								    }
+// 								}
+// 							}
+// 						}
+// 					}					
+// 				}
+// 				_ => ()			
+// 			}
+// 		}
+// 	}
+// }			
