58cb0f56b83f90e5ede5521424b855dea4e017fc
Effendi <effendi.saputra01@gmail.com>
2026-05-09 00:16:56 +0700

    added multi line support

--- src/main.rs
+++ src/main.rs
@@ -21,21 +21,20 @@ fn symlink<P: AsRef<Path>, Q: AsRef<Path>>(
 
 use std::io::{self, BufRead};
 
-fn get_user_lib(args: Option<&String>) -> Option<String> {
+fn get_user_lib<F: FnMut(String)>(args: Option<&String>, mut f: F) {
 	match args {
 		Some(c) => {
-			return Some(c.clone())
+			return f(c.clone())
 		}
 		None => {
 		    let stdin = io::stdin();
 		    for line in stdin.lock().lines() {
 		        if let Ok(content) = line {
-		            return Some(content);
+		            f(content);
 		        }
 		    }		
 		}
 	}
-    None
 }
 
 fn main() {
@@ -71,25 +70,31 @@ fn main() {
 	    					}
 						}
 						Some("register") => {
-							match get_user_lib(args.get(3)) {
-								None => {
-									println!("Atleast give us a name of your lib!");
-								}
-								Some(lib) => {
-					    			let mut path = std::path::PathBuf::from(".link");
-					    			path.push(&lib);
-					    			let mut crate_path = std::path::PathBuf::from(&first_arg);
-									crate_path.push(&lib);
-									match symlink(&crate_path, &path) {
-										Err(e) => {
-											println!("Error!: {}", e);
-										}
-										Ok(_) => {
-											println!("Successfully created a symlink for {} in {}", crate_path.display(), path.display());
-										}
+							get_user_lib(args.get(3), |lib| {
+								let mut path = std::path::PathBuf::from(".link");
+				    			path.push(&lib);
+				    			let mut crate_path = std::path::PathBuf::from(&first_arg);
+								crate_path.push(&lib);
+								match symlink(&crate_path, &path) {
+									Err(e) => {
+										println!("Error!: {}", e);
+									}
+									Ok(_) => {
+										println!("Successfully created a symlink for {} in {}", crate_path.display(), path.display());
 									}
 								}
-							}
+							});
+							// match get_user_lib(args.get(3)) {
+							// 	None => {
+							// 		println!("Atleast give us a name of your lib!");
+							// 	}
+							// 	Some(libs) => {
+							// 		println!("{}", libs);
+							// 		for lib in libs.split("\n") {
+						 //    				
+							// 		}
+							// 	}
+							// }
 						}
 						Some(s) => {
 							println!("Error: Unknown argument {}!", s);
